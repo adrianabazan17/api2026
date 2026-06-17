f9a4dbc45d79a1cb7e968c36a36d0513b326d578
adrianabazan17 <adrianacujilema578@gmail.com>
2026-06-17 13:14:34 -0500

    correccion uploads

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-import express from 'express'
+import express from 'express';
 import cors from 'cors';
-import clientesRoutes from './routes/clientes.routes.js'
-import productosRoutes from './routes/productos.routes.js'
-import authRoutes from './routes/auth.routes.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
+
+import clientesRoutes from './routes/clientes.routes.js';
+import productosRoutes from './routes/productos.routes.js';
+import authRoutes from './routes/auth.routes.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,42 +14,48 @@ const app = express();
 
 const corsOptions = {
   origin: '*',
-  methods: ['GET','POST','PUT','PATCH','DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true
-}
+};
 
-// Hacer pública la carpeta uploads (antes de las rutas)
-const uploadsPath = path.resolve(__dirname, '../uploads');
-console.log('📂 Carpeta uploads expuesta en ojo:', uploadsPath);
-app.use('/uploads', express.static(uploadsPath));
-
-
-
+// CORS
 app.use(cors(corsOptions));
+
+// JSON
 app.use(express.json());
 
+// SERVIR IMÁGENES
+const uploadsPath = path.resolve(__dirname, './uploads');
+
+console.log('📂 Carpeta uploads:', uploadsPath);
+
+app.use(
+  '/uploads',
+  express.static(uploadsPath)
+);
 
 // RUTA PRINCIPAL
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.json({
-    status: "API funcionando correctamente",
+    status: 'API funcionando correctamente',
     endpoints: [
-      "/api/clientes",
-      "/api/productos",
-      "/api/auth"
+      '/api/clientes',
+      '/api/productos',
+      '/api/login'
     ]
   });
 });
 
-// rutas
-app.use('/api', clientesRoutes)
-app.use('/api', productosRoutes)
-app.use('/api', authRoutes)
+// RUTAS API
+app.use('/api', clientesRoutes);
+app.use('/api', productosRoutes);
+app.use('/api', authRoutes);
 
-app.use((req,res,next)=>{
-  res.status(400).json({
-    message:'Endpoint not found'
-  })
-})
+// ENDPOINT NO ENCONTRADO
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Endpoint not found'
+  });
+});
 
 export default app;
